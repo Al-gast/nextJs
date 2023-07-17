fab4cf28948c334a3a8d96a067671fa3199ce17e
Al-gast <agastyo2004@gmail.com>
2023-07-17 23:56:38 +0700

    get static props

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import fs from "fs/promises";
+import path from "path";
 
-function Products() {
+function Products(props) {
+  const { products } = props;
   return (
-    <div>Products</div>
-  )
+    <div>
+      <h1>Ini adalah product page</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Products
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy.json");
+  const data = await fs.readFile(filePath);
+  const productData = JSON.parse(data);
+  return {
+    props: {
+      products: productData.products,
+    },
+  };
+}
+
+export default Products;
